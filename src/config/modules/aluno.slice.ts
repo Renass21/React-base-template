0dc73b52f0a4029f8f09f6bdae0c037424dfe1e3
Renass21 <renatodossantoss21@gmail.com>
2024-02-20 20:53:04 -0300

    extrareducers, pending, fulfilled, rejected

--- src/config/modules/aluno.slice.ts
+++ src/config/modules/aluno.slice.ts
@@ -7,14 +7,31 @@ import { LoginDTO } from "../../models/login.model";
 export const realizarLoginThunk = createAsyncThunk("aluno/realizarLogin", (data: LoginDTO) =>{
     //Chamar o login na api
     return realizarLogin(data);
-})
+});
 
 
 const alunoSlice = createSlice({
-name: "aluno",
-initialState: null as Aluno | null,
-reducers: {}
+    name: "aluno",
+    initialState: null as Aluno | null,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(realizarLoginThunk.pending, (state, action)  => {
+            console.log(action);
+            return null;
+        });
 
+        builder.addCase(realizarLoginThunk.fulfilled, (state, action) => {
+            console.log(action);
+            return action.payload;
+        });
+
+        builder.addCase(realizarLoginThunk.rejected, (state, action)  => {
+            console.log(action);
+            return null;
+        });
+        
+
+    },
 });
 
 
